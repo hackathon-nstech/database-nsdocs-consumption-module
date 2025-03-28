bbaee1c0705af393ded3e1b6a9d2f01bb4ad47e7
Andre Cadamuro Garcia <andre.garcia@kmm.com.br>
2025-03-28 14:23:06 -0300

    [add] path document via rabbitmq

--- src/infra/prisma/database-documents.service.ts
+++ src/infra/prisma/database-documents.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Body, Injectable, Logger } from '@nestjs/common';
+import { BadRequestException, Body, Injectable, Logger, Param } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { Documents } from '@prisma/client'; // Ensure Prisma types are imported
 import { UpdateCompanyConsumptionUseCase } from 'src/modules/documents/usecases/update-consumption.usecase';
@@ -68,5 +68,60 @@ export class DatabaseDocumentsService {
       this.logger.log(`Document created with ID: ${createdDocument.id}`)
   
       return createdDocument
+  }
+
+  async update(@Param('id') id: string, @Body() data: Partial<Documents>): Promise<Documents> {
+    this.logger.log(`Updating document with ID: ${id}`)
+    this.logger.debug(`Update data: ${JSON.stringify(data)}`)
+
+    const existingDocument = await this.prisma.documents.findUnique({ where: { id: BigInt(id) } });
+    if (!existingDocument) {
+      throw new Error('Document not found');
+    }
+
+    // Validate IDs
+    if (data.origin_id && !(await this.isValidRefCode(data.origin_id, 'origin'))) {
+      throw new BadRequestException('Invalid origin_id')
+    }
+    if (data.document_type_id && !(await this.isValidRefCode(data.document_type_id, 'document_type'))) {
+      throw new BadRequestException('Invalid document_type_id')
+    }
+    if (data.status_id && !(await this.isValidRefCode(data.status_id, 'status'))) {
+      throw new BadRequestException('Invalid status_id')
+    }
+
+    // Validate unique key for update
+    if (data.access_key && data.id_company) {
+      const existingDocument = await this.findByAccessKeyAndCompany(data.access_key, data.id_company);
+      if (existingDocument && existingDocument.id !== BigInt(id)) {
+        throw new BadRequestException('A document with the same access_key and id_company already exists');
+      }
+    }
+
+    if (data?.id) {
+      delete data.id // Remove id from the update data
+    }
+    const updatedDocument = await this.prisma.documents.update({
+      where: { id: BigInt(id) },
+      data,
+    });
+    this.logger.log(`Document with ID: ${id} updated successfully`)
+
+    const updateCompanyConsumption = new UpdateCompanyConsumptionUseCase(this.prisma);
+      
+    // Verifica se algum dos campos relevantes foi alterado
+    if (
+      data?.origin_id !== existingDocument?.origin_id ||
+      data?.id_company !== existingDocument?.id_company ||
+      data?.status_id !== existingDocument?.status_id
+    ) {
+      // Atualiza o consumo para os valores antigos
+      await updateCompanyConsumption.execute(existingDocument, -1);
+
+      // Atualiza o consumo para os valores novos
+      await updateCompanyConsumption.execute(updatedDocument, 1);
+    }
+
+    return updatedDocument;
   }
 }
--- src/modules/documents/documents.controller.ts
+++ src/modules/documents/documents.controller.ts
@@ -129,6 +129,38 @@ export class DocumentsController {
     return updatedDocument
   }
 
+  @Patch('async/:id')
+  async updateAsync(@Param('id') id: string, @Body() data: Partial<Documents>): Promise<Documents> {
+    this.logger.log(`Updating document with ID: ${id}`)
+    this.logger.debug(`Update data: ${JSON.stringify(data)}`)
+
+    // Validate IDs
+    if (data.origin_id && !(await this.documentsService.isValidRefCode(data.origin_id, 'origin'))) {
+      throw new BadRequestException('Invalid origin_id')
+    }
+    if (data.document_type_id && !(await this.documentsService.isValidRefCode(data.document_type_id, 'document_type'))) {
+      throw new BadRequestException('Invalid document_type_id')
+    }
+    if (data.status_id && !(await this.documentsService.isValidRefCode(data.status_id, 'status'))) {
+      throw new BadRequestException('Invalid status_id')
+    }
+
+    // Validate unique key for update
+    if (data.access_key && data.id_company) {
+      const existingDocument = await this.documentsService.findByAccessKeyAndCompany(data.access_key, data.id_company);
+      if (existingDocument && existingDocument.id !== BigInt(id)) {
+        throw new BadRequestException('A document with the same access_key and id_company already exists');
+      }
+    }
+
+    if (data?.id) {
+      delete data.id // Remove id from the update data
+    }
+    const updatedDocument = await this.documentsService.update(BigInt(id), data)
+    this.logger.log(`Document with ID: ${id} updated successfully`)
+    return updatedDocument
+  }
+
   @Delete(':id')
   async delete(@Param('id') id: string): Promise<Documents> {
     this.logger.log(`Deleting document with ID: ${id}`)
